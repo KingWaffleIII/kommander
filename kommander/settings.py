620f55b6c06b8505698527f3718b8c1bfd0e2875
KingWaffleIII <kingwaffl3iii@gmail.com>
2021-12-30 16:12:24 +0000

    Fix 404 and 500 templates not rendering

--- kommander/settings.py
+++ kommander/settings.py
@@ -38,7 +38,7 @@ else:
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = []  # e.g. ['https://kommander.planetwaffle.net', 'http://localhost:8000']
